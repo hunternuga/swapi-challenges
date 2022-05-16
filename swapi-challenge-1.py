88fe1e7ea6b69d49ffaa4ab36e8c8eab59e40dce
Hunter Nuga <94147854+hunternuga@users.noreply.github.com>
2022-05-16 10:11:20 -0600

    Add files via upload

---
+++ swapi-challenge-1.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+url = 'https://swapi.dev/api/starships'
+swapi_ships = requests.get(url)
+
+def get_pilot(pilots_url):
+    p = requests.get(pilots_url)
+    pilot = json.loads(p.text)
+    name = pilot['name']
+    return name
+
+if swapi_ships.status_code == 200:
+    swapi_s = swapi_ships.json()
+    for ships in swapi_s['results']:
+        print("The " + str(ships["name"]) + " is piloted by: ")
+        pilots = (ships['pilots'])
+        for x in range(len(pilots)):
+            piloturl = pilots[x]
+            print(" - " + str(get_pilot(piloturl)))
+        else:
+            print(" - Alot of people!")
+
+
